985255efe2216ae71f620c63d9ec888696013ed8
ArsGord <67157452+ArsGord@users.noreply.github.com>
2020-12-30 20:46:49 +0400

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите своё имя");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}");
+            Console.WriteLine("Введите количетво дней жизни");
+            string d = Console.ReadLine();
+            int days = Convert.ToInt32(d);
+            int years = 0, months = 0;
+            while (days >= 365) {
+                days -= 365;
+                years++;
+            };
+            while (days >= 30)
+            {
+                days -= 30;
+                months++;
+            };
+            Console.WriteLine($"Лет: {years}; Месяцев: {months}; Дней: {days}.");
+            Console.ReadKey();
+        }
+    }
+    // Написать программу для подсчёта возраста из количества дней, залить на гитхаб
+}
